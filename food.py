8f7c9ad192b0b43f8d7a74cf31db2868d524e264
Indrajeet Singh <indrajeetsinghmaan@gmail.com>
2022-09-27 21:14:18 +0530

    added food

---
+++ food.py
@@ -0,0 +1,13 @@
+from turtle import Turtle 
+from random import randint
+class Food:
+    def __init__(self):
+        self.snack = Turtle()
+        self.snack.penup()
+        self.snack.shape('circle')
+        self.snack.color('red')
+        self.snack.speed('fastest')
+        self.snack.goto(randint(-280, 280),randint(-280, 280))
+
+
+
