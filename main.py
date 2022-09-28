bcd3f2db9416f81418ff6434755362862d72ebb0
Indrajeet Singh <indrajeetsinghmaan@gmail.com>
2022-09-28 15:07:31 +0530

    added Wall- game over

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 from snake import Snake
 import time
 from food import Food
+from scoreboard import ScoreBoard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = ScoreBoard()
 screen.update()
 screen.listen() 
 screen.onkey(key='Up', fun=snake.up)
@@ -25,7 +27,13 @@ while True:
 
     # picking food 
     if snake.head.distance(food.snack) < 15:
+        scoreboard.update_score()
         food.relocate()
+
+    # detect collision with wall
+    if snake.head.xcor() > 270 or snake.head.xcor() < -270 or snake.head.ycor() > 280 or snake.head.ycor() < -270:
+        scoreboard.game_over()
+        break
     
 
 
